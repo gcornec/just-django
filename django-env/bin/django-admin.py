b837548c27eb73189991152c8e09bd90f975eebc
Gilles <cornec.gilles@gmail.com>
2020-06-05 09:45:48 +0200

    django crash test from justdjango

---
+++ django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gilles/Documents/GitHub/Django-Crash-Course/django-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
